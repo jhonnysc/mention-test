d455c7c3bde00497b7eec52af489927d25a013bc
jhonnyps <90932854+jhonnyps@users.noreply.github.com>
2021-12-10 17:04:42 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+interface User  {
+  id: number;
+  name: string;
+}
+
+const users: Array<User> = [
+  {
+    id: 1,
+    name: "John Doe",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+  },
+  {
+    id: 3,
+    name: "Jack Doe",
+  },
+  {
+    id: 4,
+    name: "Jill Doe",
+  },
+];
+
+
+const Modal = () => {
+  return (
+    <div>
+      <div>Modal</div>
+    </div>
+  );
+}
+
 
 function App() {
+  const [showUsers, setShowUsers] = React.useState(false);
+  const [textAreaValue, setTextAreaValue] = React.useState("");
+  const [usersSelect, setUsersSelect] = React.useState<User[]>([]);
+
+  const handleTextChange: React.TextareaHTMLAttributes<HTMLTextAreaElement>["onChange"] =
+    (event) => {
+      const text = event.target.value;
+
+      setTextAreaValue(text);
+
+      const lastCharacter = text.slice(-1);
+      
+
+      if (lastCharacter === "@") {
+        setShowUsers(true);
+      } else {
+        setShowUsers(false);
+      }
+    };
+
+
+  const handleUserClick = (userId: number) => {
+    const user = users.find((user) => user.id === userId);
+
+
+
+    if (user) {
+      setUsersSelect([...usersSelect, user]);
+      setTextAreaValue((prevValue) => `${prevValue}${user.name} `)
+    }
+    setShowUsers(false)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {usersSelect.map((user) => {
+        return (
+          <>
+            <input type='checkbox' key={user.id} />
+            <span>{user.name}</span>
+          </>
+        )
+      })}
+      <textarea onChange={handleTextChange} value={textAreaValue}></textarea>
+      <div>
+        <ul>
+          {showUsers &&
+            users.map((user) => {
+              return (
+                <li key={user.id} onClick={() => handleUserClick(user.id)}>
+                  {user.name}
+                </li>
+              );
+            })}
+        </ul>
+      </div>
     </div>
   );
 }
